8840535b11472389d2ee2ba7221ca07f6ab42cd3
PRIYANSHU KUMAR SAW <101616959+Priyanshu6268@users.noreply.github.com>
2023-09-06 23:58:02 +0530

    Update DegenToken.sol

--- DegenToken.sol
+++ DegenToken.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+contract DegenToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    address public owner;
 
-contract DegenToken is ERC20, Ownable, ERC20Burnable {
-    using SafeMath for uint256;
+    mapping(address => uint256) private balances;
+    mapping(address => mapping(address => uint256)) private allowances;
+    mapping(address => bool) private isAdmin;
+    mapping(address => uint256) private rewards;
+
     struct GameItem {
         string name;
         uint256 baseValue;
@@ -19,19 +24,96 @@ contract DegenToken is ERC20, Ownable, ERC20Burnable {
     GameItem[] public gameStore;
     mapping(address => mapping(uint256 => uint256)) public lastRedeemed;
 
-    // Events
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event RewardAdded(address indexed admin, uint256 amount);
+    event RewardClaimed(address indexed player, uint256 amount);
     event ItemRedeemed(address indexed user, string itemName, uint256 newBalance);
 
-    constructor() ERC20("Degen", "DGN") {
-        gameStore.push(GameItem("Hammer", 200, 0, 1000, 1));
-        gameStore.push(GameItem("Javline", 100, 0, 500, 2));
-        gameStore.push(GameItem("Crown", 75, 0, 2000, 1));
+    constructor() {
+        name = "Degen Token";
+        symbol = "DGN";
+        decimals = 18;
+        totalSupply = 0;
+        owner = msg.sender;
+        gameStore.push(GameItem("Sword", 300, 0, 800, 2));
+        gameStore.push(GameItem("Shield", 150, 0, 400, 1));
+        gameStore.push(GameItem("Helmet", 100, 0, 1000, 2));
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the contract owner can perform this action");
+        _;
     }
 
+    function mint(address account, uint256 amount) external onlyOwner {
+        require(account != address(0), "Invalid address");
+        require(amount > 0, "Invalid amount");
+
+        balances[account] += amount;
+        totalSupply += amount;
+
+        emit Transfer(address(0), account, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) external returns (bool) {
+        require(recipient != address(0), "Invalid address");
+        require(amount > 0, "Invalid amount");
+        require(amount <= balances[msg.sender], "Insufficient balance");
+
+        balances[msg.sender] -= amount;
+        balances[recipient] += amount;
+
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        require(spender != address(0), "Invalid address");
+
+        allowances[msg.sender][spender] = amount;
+
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {
+        require(sender != address(0), "Invalid address");
+        require(recipient != address(0), "Invalid address");
+        require(amount > 0, "Invalid amount");
+        require(amount <= balances[sender], "Insufficient balance");
+        require(amount <= allowances[sender][msg.sender], "Insufficient allowance");
+
+        balances[sender] -= amount;
+        balances[recipient] += amount;
+        allowances[sender][msg.sender] -= amount;
+
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function burn(uint256 amount) external {
+        require(amount > 0, "Invalid amount");
+        require(amount <= balances[msg.sender], "Insufficient balance");
+
+        balances[msg.sender] -= amount;
+        totalSupply -= amount;
+
+        emit Transfer(msg.sender, address(0), amount);
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+        function claimReward() external returns (uint256) {
+        uint256 amount = rewards[msg.sender];
+        require(amount > 0, "No rewards to claim");
+
+        rewards[msg.sender] = 0;
+        emit RewardClaimed(msg.sender, amount);
+
+        return amount;
+    }
 
     function redeemItem(uint256 itemId) external {
         require(itemId < gameStore.length, "Invalid item ID");
@@ -39,34 +121,32 @@ contract DegenToken is ERC20, Ownable, ERC20Burnable {
         GameItem storage item = gameStore[itemId];
 
         require(item.redeemedCount < item.maxRedeem, "Item out of stock");
-        require(balanceOf(msg.sender) >= item.baseValue, "Insufficient balance");
+        require(balances[msg.sender] >= item.baseValue, "Insufficient balance");
         require(block.timestamp > lastRedeemed[msg.sender][itemId] + 1 days, "You need to wait for 1 day between redemptions for the same item.");
 
-        uint256 price = item.baseValue.add(item.redeemedCount.mul(item.tier));
+        // uint256 price = item.baseValue.add(item.redeemedCount.mul(item.tier));
 
-        _burn(msg.sender, price);
+        // balances[msg.sender] -= price;
 
-        item.redeemedCount = item.redeemedCount.add(1);
+        // item.redeemedCount = item.redeemedCount.add(1);
         lastRedeemed[msg.sender][itemId] = block.timestamp;
 
-        emit ItemRedeemed(msg.sender, item.name, balanceOf(msg.sender));
+        emit ItemRedeemed(msg.sender, item.name, balances[msg.sender]);
     }
 
-    // Function to check redeemabl
     function getRedeemableItems() external view returns (string memory) {
-        string memory itemList = "Redeemable Items:\n";
+        string memory itemList = "Available Items:\n";
         
         for (uint i = 0; i < gameStore.length; i++) {
             GameItem storage item = gameStore[i];
-            uint256 price = item.baseValue.add(item.redeemedCount.mul(item.tier));
+            // uint256 price = item.baseValue.add(item.redeemedCount.mul(item.tier));
             
-            itemList = string(abi.encodePacked(itemList, item.name, " - ", uint2str(price), " tokens\n"));
+            itemList = string(abi.encodePacked(itemList, item.name, " - ", " tokens\n"));
         }
         
         return itemList;
     }
 
-    // Utility function to convert uint to string
     function uint2str(uint256 _i) internal pure returns (string memory str) {
         if (_i == 0) {
             return "0";
